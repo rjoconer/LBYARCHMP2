40f94d82a1091ebb7da9cc5290e877b90d19c7e7
rjoconer <raymond_james_oconer@dlsu.edu.ph>
2025-11-17 20:31:07 +0800

    refactor of sdot.asm
    changed the registers and added underscores in order to suit the specifications of macOS

--- sdot.asm
+++ sdot.asm
@@ -3,28 +3,28 @@ section .text
     default rel
     bits 64 
     
-global sdot
+global _sdot
 
-sdot:
-    xor r9, r9 ;utilize as counter
+_sdot:
+    xorps xmm1, xmm1 
+
+    xor r9, r9 ; utilize as counter
     
     L1:
-        cmp r9, rcx ;rcx = n
+        cmp r9, rdi
         jge done
         
-        ;convert value of a[r9] (rdx) to float
-        movss xmm2, [rdx + r9*4]
-        ;convert value of b[r9] (r8) to float
-        movss xmm3, [r8 + r9*4]
+        movss xmm2, [rsi + r9*4]
         
-        ;multiply element of a (in xmm2) to element of b (in xmm3)
+        movss xmm3, [rdx + r9*4]
+        
         mulss xmm2, xmm3
         
-        addss xmm1, xmm2 ;add product to return
+        addss xmm1, xmm2 ; add product to return
         
         inc r9
         jmp L1
         
     done:
-        movss xmm0, xmm1 ;move xmm1 to xmm0 (return sdot)
+        movss xmm0, xmm1 ; move xmm1 to xmm0 (return sdot)
         ret
